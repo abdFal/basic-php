2474c024a3f8dbc485757a7385381a600128e5cc
unknown <naufaldec07@gmail.com>
2023-01-10 14:46:47 +0700

    p

--- hello.php
+++ hello.php
@@ -13,7 +13,7 @@ var_dump("123");    //akan tetap dibaca string
 
 //heredoc
 echo <<<contoh
-Salah satu hal yang dapat disyukuri di tahun 2022 adalah, dapat mengikuti MIT (Mentor Intensive Training) yang diselenggarakan oleh Skilvul. Ada beberapa materi yang dibahas, diantaranya Salah satu hal yang dapat disyukuri di tahun 2022 adalah, dapat mengikuti MIT (Mentor Intensive Training)\n
+Salah satu hal yang dapat disyukuri di tahun 2022 adalah, dapat mengikuti MIT (Mentor Intensive Training) yang diselengarakan oleh SkilvulAda beberapa materi yang dibahas, diantaranya Salah satu hal yang dapat disyukuri di tahun 2022 adalah, dapat mengikuti MIT (Mentor Intensive Training)\n
 contoh;
 
 // tipe data null
@@ -21,4 +21,48 @@ $name = "nofal";
 $name = null;   //updating variable
 echo "name : ";
 print $name;
+
+// unset dan isset
+$isi = null;
+echo "Is isi Null? : ";
+var_dump(is_null($isi)); // mengecek apakah variabel null
+echo "\n";
+$contoh = "Aca";
+unset($contoh); // menghapus data variabel
+ 
+$contoh = null;
+var_dump(isset($contoh)); // mengecek apakah variabel tidak null
+
+// deklarasi array numerik
+$judul = ["RDKL ", "Alhadiid"];
+$nomor = array(10, 5, 72);
+$nomor[2] = 71; // mengubah data array dengan nomor index
+unset($nomor[1]); // menghapus data array dengan nomor index
+print ($judul[0]);
+print ($nomor[2]);
+$judul[] = " Jakarta"; // menambah array
+print ($judul[2]);
+echo "\n";
+
+// deklarasi array assosiatif (map)
+$perumahan = 
+array(
+    "id" => "7",                    // id adalah key dan 7 adalah value
+    "nama" => "Citra Indah City \n",   
+);
+echo ($perumahan["nama"]);
+
+// deklarasi array didalam array
+$sekolah = array(
+    "id" => "2",
+    "nama" => "SMA PKP Jakarta ",
+    "alamat" => array(                  // membuat array baru didalam array
+        "kecamatan" => "Kelapa dua wetan",
+        "propinsi" => "Jakarta Raya"
+    )
+);
+echo ($sekolah["nama"]);
+echo ($sekolah["alamat"]["kecamatan"]); // memanggil array dalam array
+
 ?>
+
