62a6048ab7623df8fb1b8a55610548f80c9544e8
unknown <naufaldec07@gmail.com>
2023-01-12 14:25:28 +0700

    update

--- perulangan.php
+++ perulangan.php
@@ -13,7 +13,6 @@ for ($c = 4; $c <= 10; $c++){
 }
 
 // Alternatif Loop
-
 for ($counter = 1; $counter <= 5; $counter++):
     echo "for loop $counter" . PHP_EOL;
 endfor;
@@ -29,6 +28,41 @@ while ($r >= 7){
 }
 
 // While Loop
+$o = 10;
+do {
+echo "Ini adalah do while ke-$o" . PHP_EOL;
+$o++;
+} while ($o <= 20);
 
+// Break and Continue
+$u = 1;
+while(true){
+    echo "break $u". PHP_EOL;
+    $u++;
+    if($u > 5){
+        break;      // memberhentikan loop saat kondisi terjadi
+    }};
 
+for ($counter = 1; $counter <= 10; $counter++) {
+    if ($counter % 2 !== 0) {   // loop saat kondisi bilangan genap
+    continue;
+}
+    echo "Angka $counter" . PHP_EOL;
+}
+
+// For Each Loop
+$list = [11, 12, 13];
+foreach ($list as $list){
+    echo "Num $list" . PHP_EOL;
+}
+
+//Untuk array assosiatif (Map)
+$daftar = [
+"absen_1" => "Maxmiano",
+"absen_2" => "Dialovares",
+"absen_3" => "Rodriguez"
+];
+foreach ($daftar as $key => $value) {
+    echo "$key : $value" . PHP_EOL;
+}
 ?>
